e2e6dbcc12ed0f689d4d08d6f998c727739e8fea
frankrs <frankrs3d@gmail.com>
2014-09-21 22:12:54 -0400

    1.00021
    files

---
+++ Assets/minimap/MiniMapCam.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniMapCam : MonoBehaviour {
+
+	public Transform tran;
+	public Transform pointer;
+
+	void Start(){
+		tran = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = new Vector3(tran.position.x,transform.position.y,tran.position.z);
+		pointer.position = new Vector3(tran.position.x,pointer.position.y,tran.position.z);
+		pointer.rotation = tran.rotation;
+	}
+}
