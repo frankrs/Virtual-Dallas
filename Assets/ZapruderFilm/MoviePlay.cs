fa919bdf0d55ed59fa443da9b30a30fea250d66c
frankrs <frankrs3d@gmail.com>
2014-09-24 00:15:11 -0400

    1.003
    reencoded  video

---
+++ Assets/ZapruderFilm/MoviePlay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoviePlay : MonoBehaviour {
+
+
+
+	public MovieTexture movTexture;
+
+	void Start() {
+		renderer.material.mainTexture = movTexture;
+		movTexture.Play();
+	}
+	
+
+}
