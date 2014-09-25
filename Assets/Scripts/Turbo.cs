374bf5c23c108b8a2db3184cabff97d1ba3b006f
frankrs <frankrs3d@gmail.com>
2014-09-24 22:02:40 -0400

    1.009
    holding "V" key gives you superhuman speed

---
+++ Assets/Scripts/Turbo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turbo : MonoBehaviour {
+
+	private Animator anim;
+
+	void Start(){
+		anim = GetComponent<Animator>();
+	}
+
+	void FixedUpdate(){
+		anim.SetBool("Turbo",Input.GetButton("Turbo"));
+	}
+
+}
